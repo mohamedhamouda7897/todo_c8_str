be0d9aaa9d1454581aa8d7abeab99f202554ced5
Mohamed Hamouda <mohamed.hamouda7897@gmail.com>
2023-05-06 14:45:44 +0300

    finish home layout

--- lib/home_layout/home_layout.dart
+++ lib/home_layout/home_layout.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:todo_c8_str/screens/widgets/add_task_bottom_sheet.dart';
 
-class HomeLayout extends StatelessWidget {
+class HomeLayout extends StatefulWidget {
   static const String routeName = "HomeLayout";
+
+  @override
+  State<HomeLayout> createState() => _HomeLayoutState();
+}
+
+class _HomeLayoutState extends State<HomeLayout> {
+  int index = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -11,6 +19,43 @@ class HomeLayout extends StatelessWidget {
           "Todo List",
           style: Theme.of(context).textTheme.bodyLarge,
         ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            showTaskBottomSheet();
+          },
+          shape: StadiumBorder(side: BorderSide(color: Colors.white, width: 4)),
+          child: Icon(Icons.add)),
+      bottomNavigationBar: BottomAppBar(
+        notchMargin: 8,
+        shape: CircularNotchedRectangle(),
+        child: BottomNavigationBar(
+          iconSize: 30,
+          elevation: 0.0,
+          backgroundColor: Colors.transparent,
+          currentIndex: index,
+          onTap: (value) {
+            index = value;
+            setState(() {});
+          },
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.list), label: ""),
+            BottomNavigationBarItem(icon: Icon(Icons.settings), label: ""),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void showTaskBottomSheet() {
+    showModalBottomSheet(
+      context: context,
+      isScrollControlled: true,
+      builder: (context) => Padding(
+        padding:
+            EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+        child: AddTaskBottomSheet(),
       ),
     );
   }
